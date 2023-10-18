c1283adc722bfa3a19348948dccbedd2f2dda4d7
GX-Olay <142326521+GX-Olay@users.noreply.github.com>
2023-10-18 10:57:14 -0400

    Update detekt.setup.gradle.kts

--- detekt/detekt.setup.gradle.kts
+++ detekt/detekt.setup.gradle.kts
@@ -1,33 +1,36 @@
 val detekt by configurations.creating
 
-val detektTask = tasks.register<JavaExec>("detekt") {
-    mainClass.set("io.gitlab.arturbosch.detekt.cli.Main")
-    classpath = detekt
+val detektTask =
+    tasks.register<JavaExec>("detekt") {
+        mainClass.set("io.gitlab.arturbosch.detekt.cli.Main")
+        classpath = detekt
 
-    val jvmTarget = "17"
-    val input = projectDir
-    val config = "$rootDir/kotlin-static-analysis-tools/detekt/detekt.yml"
-    val exclude = ".*/build/.*,.*/resources/.*,**/buildSrc/**,**/resources/**"
-    val report = "html:${buildDir.path}/reports/detekt.html"
-    val composeRules = "$rootDir/kotlin-static-analysis-tools/detekt/detekt-compose-0.3.0-all.jar"
-    val params = listOf(
-        "--parallel",
-        "--jvm-target",
-        jvmTarget,
-        "--input",
-        input,
-        "--config",
-        config,
-        "--excludes",
-        exclude,
-        "-r",
-        report,
-        "-p",
-        composeRules,
-    )
+        val jvmTarget = "17"
+        val input = projectDir
+        val config = "$rootDir/kotlin-static-analysis-tools/detekt/detekt.yml"
+        val exclude = ".*/build/.*,.*/resources/.*,**/buildSrc/**,**/resources/**"
+        val report = "html:${buildDir.path}/reports/detekt.html"
+        val detektCompose = "$rootDir/kotlin-static-analysis-tools/detekt/detekt-compose-0.3.0-all.jar"
+        val detektFormatting = "$rootDir/kotlin-static-analysis-tools/detekt/detekt-formatting-1.23.1.jar"
+        val params = listOf(
+            "--parallel",
+            "--jvm-target",
+            jvmTarget,
+            "--input",
+            input,
+            "--config",
+            config,
+            "--excludes",
+            exclude,
+            "-r",
+            report,
+            "--auto-correct",
+            "--plugins",
+            "$detektCompose, $detektFormatting",
+        )
 
-    args(params)
-}
+        args(params)
+    }
 
 dependencies {
     detekt("io.gitlab.arturbosch.detekt:detekt-cli:1.23.1")
