12abb829876d443950ac21a7e3c6ef7772c91851
debjyotinath <deb.projects109@gmail.com>
2023-10-20 01:46:49 +0530

    Sell and Buy New Screen migration

--- detekt/detekt.setup.gradle.kts
+++ detekt/detekt.setup.gradle.kts
@@ -13,22 +13,23 @@ val detektTask =
         val report = "html:${buildDir.path}/reports/detekt.html"
         val detektCompose = "$rootDir/kotlin-static-analysis-tools/detekt/detekt-compose-0.3.0-all.jar"
         val detektFormatting = "$rootDir/kotlin-static-analysis-tools/detekt/detekt-formatting-1.23.1.jar"
-        val params = listOf(
-            "--parallel",
-            "--jvm-target",
-            jvmTarget,
-            "--input",
-            input,
-            "--config",
-            config,
-            "--excludes",
-            exclude,
-            "-r",
-            report,
-            "--auto-correct",
-            "--plugins",
-            "$detektCompose, $detektFormatting",
-        )
+        val params =
+            listOf(
+                "--parallel",
+                "--jvm-target",
+                jvmTarget,
+                "--input",
+                input,
+                "--config",
+                config,
+                "--excludes",
+                exclude,
+                "-r",
+                report,
+                "--auto-correct",
+                "--plugins",
+                "$detektCompose, $detektFormatting",
+            )
 
         args(params)
     }
