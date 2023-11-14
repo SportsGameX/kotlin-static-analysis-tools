fc140d61605cff6592cf78d5ba99a1bd5828e51a
GX-Olay <142326521+GX-Olay@users.noreply.github.com>
2023-11-14 16:28:23 -0500

    Update static.analysis.gradle.kts

--- static.analysis.gradle.kts
+++ static.analysis.gradle.kts
@@ -1,7 +1,7 @@
 apply(from = "kotlin-static-analysis-tools/detekt/detekt.setup.gradle.kts")
 apply(from = "kotlin-static-analysis-tools/ktlint/ktlint.setup.gradle.kts")
 
-project.subprojects.first().run {
+project.run {
     val detektCheck = rootProject.tasks.plus(tasks).firstOrNull { it.name == "detekt" }
     val hookTask = tasks.firstOrNull { it.name == "preBuild" || it.name == "testClasses" }
     hookTask?.run { detektCheck?.let { dependsOn(it) } }
